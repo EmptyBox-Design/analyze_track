99d0f17791540558ae88fdbcef6f5f3d4e4e2744
bpachuca <bpachuca1@gmail.com>
2019-03-04 10:12:47 -0700

    removed value array appending to file

--- csvread.py
+++ csvread.py
@@ -1,22 +1,31 @@
 import csv
 import statistics
-import difflib
+# import difflib
 from pathlib import Path
-
+from shapely.geometry import Polygon
+# import numpy
 # helper functions
 def partitionSongName(alist):
     return alist.split("/",2)[1]
 
-def compareList(a,b):
-    # simularities = sum(a == b for a, b in zip(a, b))
-    # print("simularities: ",simularities)
-    # differences = sum(a != b for a, b in zip(a, b))
+def getAreaFromPolygon(list_A, list_B):
+    polygon_points = [] #creates a empty list where we will append the points to create the polygon
+    for val in list_A:
+        polygon_points.append([val[0],val[1]])
+    for val in list_B[::-1]:
+        polygon_points.append([val[0],val[1]])
+    for val in list_A[0:1]:
+        polygon_points.append([val[0],val[1]])
+    
+    polygon = Polygon(polygon_points)
+    return polygon.area
+
+def compareList(a,b,c,d):
     deviationA = statistics.stdev(a)
-    # print("deviationA",deviationA)
-    # print("abs",deviationA)
     deviationB = statistics.stdev(b)
-    # print("differences: ", differences)
-    return abs(deviationA - deviationB)
+    deviationDifference = abs(deviationA - deviationB)
+    polygonDifference = getAreaFromPolygon(c,d)
+    return deviationDifference * polygonDifference
 
 def __createGlobalListFromDirectory__ (directory):
     dataList = []
@@ -25,21 +34,24 @@ def __createGlobalListFromDirectory__ (directory):
     for path in pathlist:
         # filename pathing
         path_in_str = str(path)
-        # print(path_in_str)
         # read the file in directory as a csv
         with open(path_in_str, newline='') as csvfile:
             spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
             # array of values
             valueArray = []
+            # tuples for calculating area
+            coordArray = []
             # empty dict for creating dump file
             d = dict()
             # iterate through each row and fill in dump file with array values
             for row in spamreader:
                 if(row[1] != 'value'):
                     valueArray.append(float(row[1]))
+                    coordArray.append((float(row[0]), float(row[1])))
             # field names added to object
             d['songName'] = partitionSongName(path_in_str)
             d['values'] = valueArray
+            d['coordArray'] = coordArray
             d['songList'] = []
             # append object to global list
             dataList.append(d.copy())
@@ -50,15 +62,12 @@ def __createSortedList__(rawList):
     for d in rawList:
         for e in rawList:
             if(d['songName'] != e['songName']):
-                # sm = difflib.SequenceMatcher(None,d['values'],e['values'])
-                sm = compareList(d['values'], e['values'])
-                # obj = [e['songName'], sm.ratio()]
-                # print("list compare return: ",sm)
+                sm = compareList(d['values'], e['values'], d['coordArray'], e['coordArray'])
                 obj = [e['songName'], sm]
                 d['songList'].append(obj)
 
     for j in rawList:
-        sort = sorted(j['songList'], key=lambda song:song[1], reverse=True)
+        sort = sorted(j['songList'], key=lambda song:song[1])
         j['songList'] = sort
 
     print("done sorting data")
@@ -66,7 +75,7 @@ def __createSortedList__(rawList):
 
 def __writeDataToCSV__(sortedData):
     writeCSV = list()
-    header = ["song_name","average","standard_deviation","variance","rank_1_name","rank_1_score","rank_2_name","rank_2_score","rank_3","rank_3_score"]
+    header = ["song_name","average","standard_deviation","variance","rank_1_name","rank_1_score","rank_2_name","rank_2_score","rank_3_name","rank_3_score"]
     writeCSV.append(header)
 
     for c in sortedData:
