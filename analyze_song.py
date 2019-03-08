265e2eabfcafdb5aa8c3d90b19e632741f36f777
bpachuca <bpachuca1@gmail.com>
2019-03-08 11:13:07 -0700

    active bug: non-repeatable data

--- analyze_song.py
+++ analyze_song.py
@@ -1,39 +1,76 @@
 import csv
 import statistics
+from csvWrite import writeData
+import pygame
+from pygame import mixer
 
-def compareList(a,deviationB):
-    deviationA = statistics.stdev(a)
-    deviationDifference = abs(deviationA - deviationB)
-    return deviationDifference
+def compareCVs(a,b):
+    return abs(a - b)
 
+def splitSongPath(inputSongPath):
+    print('inputSongPath: ', inputSongPath[0].split(".")[0])
+    return inputSongPath[0].split(".")[0]
+
+# inputs new song to be compared against data.csv
+# this returns an array of spectrum results as a list
+# this will fire when a user interacts with the pygame button
+# sending a song file to be compared and matched with
 def createSongArray (song):
-    currentSongArray = []
-
-    with open(song, newline='') as songFile:
-        f = csv.reader(songFile, delimiter=",",quotechar='|')
+    # print('song: ', song)
+    # dump array
+    # currentSongArray = []
+    # init librosa comparison 
+    lb = writeData()
+    inputSongArray = lb.createSongData(song)
+    # print('inputSongArray: ', inputSongArray)
+    standarDeviation = statistics.stdev(inputSongArray)
+    print('standarDeviation: ', standarDeviation)
+    mean = statistics.mean(inputSongArray)
+    print('mean: ', mean)
+    coefficientVariance = (standarDeviation/mean) *100
+    print('coefficientVariance: ', coefficientVariance)
+    # temp file created to house song data to be compared
+    # not sure if I want to write this to disk
+    # or hold it in temp memory
+    # with open("./tempFile/"+song, newline='') as songFile:
+    #     f = csv.reader(songFile, delimiter=",",quotechar='|')
         
-        for row in f:
-            if(row[1] != 'value'):
-                currentSongArray.append(float(row[1]))
+    #     for row in f:
+    #         if(row[1] != 'value'):
+    #             currentSongArray.append(float(row[1]))
+    # match = matchAgainstSongs(coefficientVariance)
+    # playSong(match)
 
-    return currentSongArray
-
-def matchAgainstSongs (currentSongArray):
+def matchAgainstSongs (coefficientVariance):
     with open("data.csv", newline='') as csvfile:
         f = csv.reader(csvfile, delimiter=',', quotechar='|')
 
         finalCompareList = []
-
+        # skip header row
         for row in f:
             if(row[2] != "standard_deviation"):
-
-                compare = compareList(currentSongArray, float(row[2]))
+                # input input songs CV, and comparision songs CV
+                # output is the difference
+                print("what we are matching against: ",row[0], float(row[4]))
+                compare = compareCVs(coefficientVariance, float(row[4]))
+                # append to list for sorting 
                 finalCompareList.append((row[0], compare))
-
+        # sort in decending order
         sortedByLowestRank = sorted(finalCompareList, key=lambda tup: tup[1])
-
+        # print('sortedByLowestRank: ', sortedByLowestRank)
+        # return lowest value
         return sortedByLowestRank[0]
 
-createdSongArrays = createSongArray('songData/all_star.csv')
-comparasionMatch = matchAgainstSongs(createdSongArrays)
-print('comparasionMatch: ', comparasionMatch)
+def playSong (inputSongPath):
+    pygame.init()
+    pygame.display.set_mode((200,100))
+    pygame.mixer.music.load("/Users/bamforion/Documents/Processing/analyze_track/data/"+splitSongPath(inputSongPath)+".mp3")
+    pygame.mixer.music.play(0)
+
+    clock = pygame.time.Clock()
+    clock.tick(10)
+    while pygame.mixer.music.get_busy():
+        pygame.event.poll()
+        clock.tick(10)
+
+createSongArray("/Users/bamforion/Documents/Processing/analyze_track/data/twenty_one_pilots__jar.mp3")
