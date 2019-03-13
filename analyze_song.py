a306e2d271b6fa6724a0304d665e3a4a9f518811
bpachuca <bpachuca1@gmail.com>
2019-03-13 17:47:26 -0600

    Appends text after button push

--- analyze_song.py
+++ analyze_song.py
@@ -7,6 +7,7 @@ import sys
 class pygameObject:
 
     def __init__(self):
+
         pygame.init()
         self.fps = 60
         self.clock = pygame.time.Clock()
@@ -19,75 +20,38 @@ class pygameObject:
         self.buttonYPos = self.screenHeight/2 - self.buttonHeight/2
         self.size = [self.screenWidth, self.screenHeight]
         self.bg = [255, 255, 255]
-        self.startPygame()
-
-    def splitSongPath(inputSongPath):
-        return inputSongPath[0].split(".")[0].replace(".mp3", ".ogg")
-
-    def startPygame(self):
-        # init screen 
-        screen = pygame.display.set_mode(self.size)
-        # creates button object
-        button = pygame.Rect(self.buttonXPos, self.buttonYPos, self.buttonWidth, self.buttonHeight)
-
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    return False
-
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    mouse_pos = event.pos  # gets mouse position
-
-                    # checks if mouse position is over the button
-                    # use collider function
-                    if button.collidepoint(mouse_pos):
-                        # prints current location of mouse
-                        print('button was pressed at {0}'.format(mouse_pos))
-                        matchSongs()
-                        
-            # fill in screen
-            screen.fill(self.bg)
-            # draw button
-            pygame.draw.rect(screen, [255, 0, 0], button)  # draw button
-
-            pygame.display.update()
-            self.clock.tick(self.fps)
-
-        pygame.quit()
-        sys.exit
-
-    # input song path from data.csv and plays song
-    # using pygame mixer
-    def playSong (self,inputSongPath):
-        # splits song path from .csv extension and returns song name
-        songPath = "/Users/bamforion/Documents/Processing/analyze_track/song_tracks_ogg/"+ self.splitSongPath(inputSongPath) + ".ogg"
-
-        pygame.mixer.music.load(songPath)
-        pygame.mixer.music.play(0)
-
-        # while pygame.mixer.music.get_busy():
-        #     pygame.event.poll()
-        #     self.clock.tick(self.fps)
-
-class matchSongs:
-
-    def __init__(self):
         self.sortedByLowestRank = []
         self.finalCompareList = []
         self.standardDeviation = 0
         self.mean = 0
         self.coefficientVariance = 0
-        self.startMathcing()
+
+    def splitSongPath(self,inputSongPath):
+        return inputSongPath[0].split(".")[0].replace(".mp3", ".ogg")
+
     # returns absolute value of the songs
     # coefficients of deviation
     def compareCVs(self,a,b):
         return abs(a - b)
 
     # start the thing
-    def startMathcing(self):
+    def startMatching(self):
         song = self.getSong()
         matchedSong = self.createSongArray(song)
-        pygameObject.playSong(pygameObject,matchedSong)      
+        py.playSong(matchedSong) 
+
+    # inputs text, x coordinate, y coordinate
+    # clears screen at the top 
+    # appends new text to pygame
+    def appendText(self, text, locX, locY):
+        basicfont = pygame.font.SysFont(None, 48)
+        text = basicfont.render(text, True, (255, 0, 0), (255, 255, 255))
+        textrect = text.get_rect()
+        textrect.centerx = locX
+        textrect.centery = locY
+        self.screen.blit(text, textrect)
+        pygame.display.update()
+        self.clock.tick(self.fps)
 
     # returns song from sample folder
     def getSong(self):
@@ -100,7 +64,6 @@ class matchSongs:
     def createSongArray (self,song):
         # dump array
         # init librosa comparison 
-        
         lb = librosaGetFeatures(song)
         inputSongArray = lb.getSpectrumArray()
         self.standarDeviation = statistics.stdev(inputSongArray)
@@ -124,4 +87,55 @@ class matchSongs:
             # return lowest value
             return self.sortedByLowestRank[0]
 
+    def startPygame(self):
+        # init screen 
+        self.screen = pygame.display.set_mode(self.size)
+        # creates button object
+        button = pygame.Rect(self.buttonXPos, self.buttonYPos, self.buttonWidth, self.buttonHeight)
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return False
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_pos = event.pos  # gets mouse position
+                    # checks if mouse position is over the button
+                    # use collider function
+                    if button.collidepoint(mouse_pos):
+                        self.startMatching()
+
+            # fill in screen
+            self.screen.fill(self.bg)
+            # draw button
+            pygame.draw.rect(self.screen, [255, 0, 0], button)  # draw button
+            self.appendText("Press Button to Match Song", self.screen.get_rect().centerx, 50)
+
+        pygame.quit()
+        sys.exit
+
+    def parseSongInfo(self,matchedSong):
+        parseSong = matchedSong[0].split("__",2)
+        artist = parseSong[0].replace("_", " ")
+        song = parseSong[1].replace(".csv", " ")
+        self.screen.fill(self.bg, (0,0,self.screenWidth,200))
+        self.appendText(artist, self.screen.get_rect().centerx, 50)
+        self.appendText(song, self.screen.get_rect().centerx, 100)
+
+    # input song path from data.csv and plays song
+    # using pygame mixer
+    def playSong (self,inputSongPath):
+        # splits song path from .csv extension and returns song name
+        songPath = "/Users/bamforion/Documents/Processing/analyze_track/song_tracks_ogg/"+ self.splitSongPath(inputSongPath) + ".ogg"
+        self.parseSongInfo(inputSongPath)
+
+        pygame.mixer.music.load(songPath)
+        pygame.mixer.music.play(0)
+
+        while pygame.mixer.music.get_busy():
+            pygame.event.poll()
+            self.clock.tick(self.fps)
+
 py = pygameObject()
+py.startPygame.__init__()
+py.startPygame()
